c64dd9ed839149393e175d10a74a16040fe05e07
Giovana <giivictoria26@gmail.com>
2024-06-28 11:04:15 -0300

    alteração de alguns detalhes e começando a estilizar a parte de serviços

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Equipe
 
 function showText(id) {
-    var texts = window.document.querySelectorAll(`.text`);
+    var texts = document.querySelectorAll('.text');
     texts.forEach(function(text) {
-        text.style.display = `none`;
+        text.style.display = 'none';
     });
 
-    var textToShow = window.document.getElementById(id);
-    textToShow.style.display = `block`;
+    var textToShow = document.getElementById(id);
+    textToShow.style.display = 'block';
+    textToShow.scrollIntoView({ block: 'center' });
 }
